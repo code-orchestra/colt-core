4e31fd2a882bc3e41badbdf1f9d254359f1dd90b
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-08-01 12:04:05 +0700

    [COLT-341]

---
+++ src/codeOrchestra/groovyfx/ast/AnyClass2.groovy
@@ -0,0 +1,15 @@
+package codeOrchestra.groovyfx.ast
+
+import groovyx.javafx.beans.FXBindable
+
+
+/**
+ * @author Eugene Potapenko
+ */
+class AnyClass2 {
+
+    @FXBindable Map<String, String> date;
+
+    static void main(String[] args){
+    }
+}
