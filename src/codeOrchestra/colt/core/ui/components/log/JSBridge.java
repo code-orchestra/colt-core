d6ba3b4c2fd3bab6caa8e50f211ae7cfff5e4239
Eugene Potapenko <potapenko@codeorchestra.com>
2013-08-08 18:23:23 +0200

    html log in pogress

---
+++ src/codeOrchestra/colt/core/ui/components/log/JSBridge.java
@@ -0,0 +1,21 @@
+package codeOrchestra.colt.core.ui.components.log;
+
+import netscape.javascript.JSObject;
+import javafx.scene.web.WebEngine;
+
+/**
+ * @author Eugene Potapenko
+ */
+public abstract class JSBridge {
+    private WebEngine engine;
+
+    protected JSBridge(WebEngine engine) {
+        this.engine = engine;
+        JSObject win = (JSObject) engine.executeScript("window");
+        win.setMember("app", this);
+    }
+
+    abstract void resize(int height);
+
+    abstract void scroll(int pos);
+}
--- src/codeOrchestra/colt/core/ui/components/log/LogWebView.groovy
+++ src/codeOrchestra/colt/core/ui/components/log/LogWebView.groovy
@@ -1,26 +1,46 @@
 package codeOrchestra.colt.core.ui.components.log
 
+import javafx.beans.value.ChangeListener
+import javafx.beans.value.ObservableValue
 import javafx.collections.ObservableList as OL
+import javafx.concurrent.Worker
 import javafx.scene.control.ScrollPane
+import javafx.scene.layout.HBox
+import javafx.scene.layout.Pane
+import javafx.scene.layout.StackPane
+import javafx.scene.shape.Rectangle
+import javafx.scene.web.PopupFeatures
+import javafx.scene.web.WebEngine
 import javafx.scene.web.WebView
-
-import static javafx.scene.control.ScrollPane.ScrollBarPolicy.NEVER
+import javafx.util.Callback
 
 /**
  * @author Eugene Potapenko
  */
-class LogWebView extends ScrollPane {
+class LogWebView extends HBox {
 
-    WebView webView = new WebView()
-
+    WebView webView = new WebView(contextMenuEnabled: false)
     private OL<LogMessage> logMessages
 
     void setLogList(OL<LogMessage> logMessages) {
         this.logMessages = logMessages
+
         String htmlPage = this.class.getResource("html/log-webview.html").toExternalForm()
-        webView.engine.load(htmlPage)
-        webView.prefWidthProperty().bind(this.widthProperty())
-        setHbarPolicy(NEVER)
-        setContent(webView)
+        WebEngine engine = webView.engine
+        engine.documentProperty().addListener({ o, oldValue, newValue ->
+            new JSBridge(engine) {
+                @Override
+                void resize(int height) {
+                    println "height = $height"
+                }
+
+                @Override
+                void scroll(int pos) {
+
+                }
+            }
+        } as ChangeListener)
+        engine.load(htmlPage)
+        children.add(webView)
     }
 }
