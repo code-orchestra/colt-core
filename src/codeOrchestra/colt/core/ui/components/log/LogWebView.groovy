3d32f0b2c4ed12d3434c940b931f55fc27146dc6
Eugene Potapenko <potapenko@codeorchestra.com>
2013-08-09 15:37:33 +0200

    html log in pogress

--- src/codeOrchestra/colt/core/ui/components/log/LogWebView.groovy
+++ src/codeOrchestra/colt/core/ui/components/log/LogWebView.groovy
@@ -1,23 +1,24 @@
 package codeOrchestra.colt.core.ui.components.log
 
+import codeOrchestra.colt.core.logging.Level
 import javafx.beans.value.ChangeListener
-import javafx.beans.value.ObservableValue
+import javafx.collections.ListChangeListener
 import javafx.collections.ObservableList as OL
-import javafx.concurrent.Worker
-import javafx.scene.control.ScrollPane
-import javafx.scene.layout.HBox
-import javafx.scene.layout.Pane
-import javafx.scene.layout.StackPane
-import javafx.scene.shape.Rectangle
-import javafx.scene.web.PopupFeatures
+import javafx.event.EventHandler
+import javafx.scene.layout.VBox
 import javafx.scene.web.WebEngine
+import javafx.scene.web.WebEvent
 import javafx.scene.web.WebView
-import javafx.util.Callback
+import netscape.javascript.JSObject
+
+import java.util.regex.Pattern
+
+import static codeOrchestra.colt.core.logging.Level.*
 
 /**
  * @author Eugene Potapenko
  */
-class LogWebView extends HBox {
+class LogWebView extends VBox {
 
     WebView webView = new WebView(contextMenuEnabled: false)
     private OL<LogMessage> logMessages
@@ -35,7 +36,7 @@ class LogWebView extends HBox {
             new JSBridge(engine) {
                 @Override
                 void resize(int height) {
-                    println "height = $height"
+                    //println "height = $height"
                 }
 
                 @Override
@@ -43,8 +44,54 @@ class LogWebView extends HBox {
 
                 }
             }
+
+            clear()
+            logMessages.each {
+                addLogMessage(it)
+            }
         } as ChangeListener)
         engine.load(htmlPage)
         children.add(webView)
+
+        logMessages.addListener({ ListChangeListener.Change<? extends LogMessage> c ->
+            if(c.wasRemoved()){
+                clear()
+            }
+            c.getAddedSubList().each {
+                addLogMessage(it)
+            }
+        } as ListChangeListener)
+
+        engine.onAlert = new EventHandler<WebEvent<String>>() {
+            @Override
+            void handle(WebEvent<String> event) {
+                println("alert >> "+ event.data)
+            }
+        }
+    }
+
+    void addLogMessage(LogMessage message){
+        if (message) {
+            String messageText = message.message
+            String level = "trace"
+            switch (message.level) {
+                case FATAL:
+                case ERROR:
+                    level = "error"; break
+                case WARN:
+                    level = "warning"; break
+                case INFO:
+                    level = "info"; break
+            }
+            String source = message.source
+            JSObject window = (JSObject) webView.engine.executeScript("window")
+            window.call("addLogMessage", messageText, level, source)
+        }
+    }
+
+    void clear(){
+        JSObject window = (JSObject) webView.engine.executeScript("window")
+        window.call("clear")
+//        webView.engine.executeScript("clear()")
     }
 }
