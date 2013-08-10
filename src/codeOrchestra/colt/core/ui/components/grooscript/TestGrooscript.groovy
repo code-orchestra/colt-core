47de9f542fc00a61f673f4732b5e1c22e4fe36e3
Eugene Potapenko <potapenko@codeorchestra.com>
2013-08-10 13:56:25 +0200

    html log in pogress

---
+++ src/codeOrchestra/colt/core/ui/components/grooscript/TestGrooscript.groovy
@@ -0,0 +1,11 @@
+package codeOrchestra.colt.core.ui.components.grooscript
+
+/**
+ * @author Eugene Potapenko
+ */
+class TestGrooscript {
+
+    void myMethod(String hello){
+
+    }
+}
--- src/codeOrchestra/colt/core/ui/components/log/LogWebView.groovy
+++ src/codeOrchestra/colt/core/ui/components/log/LogWebView.groovy
@@ -1,6 +1,8 @@
 package codeOrchestra.colt.core.ui.components.log
 
+import javafx.application.Platform
 import javafx.beans.value.ChangeListener
+import javafx.collections.FXCollections
 import javafx.collections.ListChangeListener
 import javafx.collections.ObservableList as OL
 import javafx.event.EventHandler
@@ -19,49 +21,50 @@ import static codeOrchestra.colt.core.logging.Level.*
  */
 class LogWebView extends VBox {
 
-    WebView webView = new WebView(contextMenuEnabled: false)
-    private OL<LogMessage> logMessages
+    private WebView webView = new WebView(contextMenuEnabled: false)
+    final OL<LogMessage> logMessages = FXCollections.observableArrayList()
+    private boolean inited;
 
-    OL<LogMessage> getLogList() {
-        return logMessages
-    }
-
-    void setLogList(OL<LogMessage> logMessages) {
-        this.logMessages = logMessages
-
+    LogWebView() {
         String htmlPage = this.class.getResource("html/log-webview.html").toExternalForm()
         WebEngine engine = webView.engine
         engine.documentProperty().addListener({ o, oldValue, newValue ->
             new JSBridge(engine) {
                 @Override
                 void resize(int height) {
-                    //println "height = $height"
+                    println "height = $height"
                 }
             }
-
             clear()
-            logMessages.each {
-                addLogMessage(it)
+            Platform.runLater{
+                logMessages.each {
+                    addLogMessage(it)
+                }
+                inited = true;
             }
         } as ChangeListener)
         engine.load(htmlPage)
         children.add(webView)
 
         logMessages.addListener({ ListChangeListener.Change<? extends LogMessage> c ->
-            while (c.next()) {
-                if (c.wasRemoved()) {
-                    clear()
-                } else if (c.wasPermutated()) {
-                    println "permutated"
-                } else if (c.wasUpdated()) {
-                    println "updated"
-                } else {
-                    c.getAddedSubList().each {
-                        addLogMessage(it)
+            Platform.runLater{
+                if (inited) {
+                    while (c.next()) {
+                        if (c.wasRemoved()) {
+                            clear()
+                        } else if (c.wasPermutated()) {
+                            println "permutated"
+                        } else if (c.wasUpdated()) {
+                            println "updated"
+                        } else {
+                            c.getAddedSubList().each {
+                                addLogMessage(it)
+                            }
+                        }
                     }
+                    filter()
                 }
             }
-            filter()
         } as ListChangeListener)
 
         engine.onAlert = new EventHandler<WebEvent<String>>() {
@@ -76,10 +79,12 @@ class LogWebView extends VBox {
             void handle(KeyEvent event) {
                 if (event.controlDown) {
                     if (event.code == KeyCode.SPACE) {
-                        logMessages.add(new LogMessage("com.codeOrchestra.*:8", WARN, """ListView Selection / Focus APIs To track selection and focus, it is necessary to become familiar with the SelectionModel and FocusModel classes. A ListView has at most one instance of each of these classes, available from selectionModel and focusModel properties respectively. Whilst it is possible to use this API to set a new selection model, in most circumstances this is not necessary - the default selection and focus models should work in most circumstances. The default SelectionModel used when instantiating a ListView is an implementation of the MultipleSelectionModel abstract class. However, as noted in the API documentation for the selectionMode property, the default value is SelectionMode.SINGLE. To enable multiple selection in a default ListView instance, it is therefore necessary to do the following: listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);""", 10, ""))
-                        logMessages.add(new LogMessage("com.codeOrchestra.*:8", ERROR, """ListView Selection / Focus APIs To track selection and focus, it is necessary to become familiar with the SelectionModel and FocusModel classes. A ListView has at most one instance of each of these classes, available from selectionModel and focusModel properties respectively. Whilst it is possible to use this API to set a new selection model, in most circumstances this is not necessary - the default selection and focus models should work in most circumstances. The default SelectionModel used when instantiating a ListView is an implementation of the MultipleSelectionModel abstract class. However, as noted in the API documentation for the selectionMode property, the default value is SelectionMode.SINGLE. To enable multiple selection in a default ListView instance, it is therefore necessary to do the following: listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);""", 10, ""))
-                        logMessages.add(new LogMessage("com.codeOrchestra.*:8", INFO, """ListView Selection / Focus APIs To track selection and focus, it is necessary to become familiar with the SelectionModel and FocusModel classes. A ListView has at most one instance of each of these classes, available from selectionModel and focusModel properties respectively. Whilst it is possible to use this API to set a new selection model, in most circumstances this is not necessary - the default selection and focus models should work in most circumstances. The default SelectionModel used when instantiating a ListView is an implementation of the MultipleSelectionModel abstract class. However, as noted in the API documentation for the selectionMode property, the default value is SelectionMode.SINGLE. To enable multiple selection in a default ListView instance, it is therefore necessary to do the following: listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);""", 10, ""))
-                        logMessages.add(new LogMessage("com.codeOrchestra.*:8", TRACE, """ListView Selection / Focus APIs To track selection and focus, it is necessary to become familiar with the SelectionModel and FocusModel classes. A ListView has at most one instance of each of these classes, available from selectionModel and focusModel properties respectively. Whilst it is possible to use this API to set a new selection model, in most circumstances this is not necessary - the default selection and focus models should work in most circumstances. The default SelectionModel used when instantiating a ListView is an implementation of the MultipleSelectionModel abstract class. However, as noted in the API documentation for the selectionMode property, the default value is SelectionMode.SINGLE. To enable multiple selection in a default ListView instance, it is therefore necessary to do the following: listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);""", 10, ""))
+                        (10).times {
+                            logMessages.add(new LogMessage("com.codeOrchestra.*:8", WARN, """ListView Selection / Focus APIs To track selection and focus, it is necessary to become familiar with the SelectionModel and FocusModel classes. A ListView has at most one instance of each of these classes, available from selectionModel and focusModel properties respectively. Whilst it is possible to use this API to set a new selection model, in most circumstances this is not necessary - the default selection and focus models should work in most circumstances. The default SelectionModel used when instantiating a ListView is an implementation of the MultipleSelectionModel abstract class. However, as noted in the API documentation for the selectionMode property, the default value is SelectionMode.SINGLE. To enable multiple selection in a default ListView instance, it is therefore necessary to do the following: listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);""", 10, ""))
+                            logMessages.add(new LogMessage("com.codeOrchestra.*:8", ERROR, """ListView Selection / Focus APIs To track selection and focus, it is necessary to become familiar with the SelectionModel and FocusModel classes. A ListView has at most one instance of each of these classes, available from selectionModel and focusModel properties respectively. Whilst it is possible to use this API to set a new selection model, in most circumstances this is not necessary - the default selection and focus models should work in most circumstances. The default SelectionModel used when instantiating a ListView is an implementation of the MultipleSelectionModel abstract class. However, as noted in the API documentation for the selectionMode property, the default value is SelectionMode.SINGLE. To enable multiple selection in a default ListView instance, it is therefore necessary to do the following: listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);""", 10, ""))
+                            logMessages.add(new LogMessage("com.codeOrchestra.*:8", INFO, """ListView Selection / Focus APIs To track selection and focus, it is necessary to become familiar with the SelectionModel and FocusModel classes. A ListView has at most one instance of each of these classes, available from selectionModel and focusModel properties respectively. Whilst it is possible to use this API to set a new selection model, in most circumstances this is not necessary - the default selection and focus models should work in most circumstances. The default SelectionModel used when instantiating a ListView is an implementation of the MultipleSelectionModel abstract class. However, as noted in the API documentation for the selectionMode property, the default value is SelectionMode.SINGLE. To enable multiple selection in a default ListView instance, it is therefore necessary to do the following: listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);""", 10, ""))
+                            logMessages.add(new LogMessage("com.codeOrchestra.*:8", TRACE, """ListView Selection / Focus APIs To track selection and focus, it is necessary to become familiar with the SelectionModel and FocusModel classes. A ListView has at most one instance of each of these classes, available from selectionModel and focusModel properties respectively. Whilst it is possible to use this API to set a new selection model, in most circumstances this is not necessary - the default selection and focus models should work in most circumstances. The default SelectionModel used when instantiating a ListView is an implementation of the MultipleSelectionModel abstract class. However, as noted in the API documentation for the selectionMode property, the default value is SelectionMode.SINGLE. To enable multiple selection in a default ListView instance, it is therefore necessary to do the following: listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);""", 10, ""))
+                        }
                         println "logMessages = ${logMessages.size()}"
                     } else if (event.code == KeyCode.BACK_SPACE) {
                         logMessages.clear()
