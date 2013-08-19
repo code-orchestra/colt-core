d4f4e8e746eb242b79fdea89ee87ea55da43d7c1
Dima Kruk <dima.kruk@gmail.com>
2013-08-19 13:14:50 +0200

    [CAS-252] refactoring

---
+++ src/codeOrchestra/colt/core/tracker/GAController.groovy
@@ -0,0 +1,98 @@
+package codeOrchestra.colt.core.tracker
+
+import javafx.beans.property.ObjectProperty
+import javafx.beans.value.ChangeListener
+import javafx.event.ActionEvent
+import javafx.event.Event
+import javafx.event.EventHandler
+import javafx.event.EventTarget
+import javafx.event.EventType
+import javafx.scene.input.InputMethodEvent
+import javafx.stage.Stage
+import javafx.scene.Node as FXNode
+
+/**
+ * @author Dima Kruk
+ */
+class GAController {
+    private static final GAController instance = new GAController();
+
+    static GAController getInstance() {
+        return instance
+    }
+
+    GATracker tracke =  GATracker.instance
+
+    Stage curStage
+
+    private ObjectProperty<FXNode> pageContainer
+
+    class GAPageInfo {
+        String url
+        String title
+
+        GAPageInfo(String url, String title) {
+            this.url = url
+            this.title = title
+        }
+    }
+
+    HashMap<FXNode, GAPageInfo> pagesMap = new HashMap<>()
+
+    class GAEventInfo {
+        String category
+        String action
+
+        GAEventInfo(String category, String action) {
+            this.category = category
+            this.action = action
+        }
+    }
+
+    HashMap<EventTarget, GAEventInfo> eventsMap = new HashMap<>()
+
+    private GAController() {
+    }
+
+    private EventHandler<Event> eventHandler = new EventHandler<Event>() {
+        @Override
+        void handle(Event it) {
+//            println("it -> " + it)
+            if (it instanceof ActionEvent) {
+                println "action > ${it.target}"
+
+                if (eventsMap.containsKey(it.target)) {
+                    GAEventInfo info = eventsMap[it.target]
+                    tracke.trackEventWithPage(info.category, info.category)
+                }
+            }
+
+            if (it instanceof InputMethodEvent) {
+                println "focus > $it"
+            }
+        }
+    }
+
+    void start(Stage stage) {
+        curStage?.removeEventFilter(EventType.ROOT, eventHandler)
+
+        curStage = stage
+        curStage.addEventFilter(EventType.ROOT, eventHandler)
+    }
+
+    void registerEvent(EventTarget target, String category, String action) {
+        eventsMap[target] = new GAEventInfo(category, action)
+    }
+
+    void setPageContainer(ObjectProperty<FXNode> container) {
+        pageContainer = container
+        pageContainer.addListener({observableValue, FXNode oldNode, FXNode newNode ->
+            GAPageInfo info = pagesMap[newNode]
+            tracke.trackPageView(info.url, info.title)
+        } as ChangeListener)
+    }
+
+    void registerPage(FXNode node, String url, String title) {
+        pagesMap[node] = new GAPageInfo(url, title)
+    }
+}
