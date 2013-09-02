e02f2722f6b184b286d007c37548db553b7c9248
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2013-09-02 15:59:06 +0200

    welcome screen in progress

---
+++ src/codeOrchestra/colt/core/ui/components/welcomeScreen/WelcomeScreen.groovy
@@ -0,0 +1,12 @@
+package codeOrchestra.colt.core.ui.components.welcomeScreen
+
+import javafx.scene.layout.Pane
+
+/**
+ * @author Eugene Potapenko
+ */
+class WelcomeScreen extends Pane {
+    WelcomeScreen() {
+
+    }
+}
