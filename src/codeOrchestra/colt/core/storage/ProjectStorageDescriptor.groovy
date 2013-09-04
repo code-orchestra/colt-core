5c343fab8d594d8c43dc1358823b41168b74d6f2
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-09-04 13:51:04 +0400

    [CJ-69]

--- src/codeOrchestra/colt/core/storage/ProjectStorageDescriptor.groovy
+++ src/codeOrchestra/colt/core/storage/ProjectStorageDescriptor.groovy
@@ -8,4 +8,9 @@ class ProjectStorageDescriptor {
     String projectPath;
     String storageSubDir;
 
+    ProjectStorageDescriptor(String projectPath, String storageSubDir) {
+        this.projectPath = projectPath
+        this.storageSubDir = storageSubDir
+    }
+
 }
--- src/codeOrchestra/colt/core/storage/ProjectStorageManager.groovy
+++ src/codeOrchestra/colt/core/storage/ProjectStorageManager.groovy
@@ -1,5 +1,10 @@
 package codeOrchestra.colt.core.storage
 
+import codeOrchestra.util.FileUtils
+import codeOrchestra.util.ProjectHelper
+import codeOrchestra.util.StringUtils
+import groovy.util.slurpersupport.GPathResult
+
 /**
  * @author Alexander Eliseyev
  */
@@ -12,15 +17,58 @@ class ProjectStorageManager {
         if (!coltDir.exists()) {
             coltDir.mkdir()
         }
-        coltDir
+        return coltDir
     }
 
-    private List<ProjectStorageDescriptor> getStorageDescriptors() {
-        null; // TODO: implement
+    private File getStorageDescriptorsFile() {
+        new File(getOrCreateColtDir(), "storage.xml")
     }
 
-    File getProjectStorageDir() {
-        null; // TODO: implement
+    List<ProjectStorageDescriptor> getStorageDescriptors() {
+        File descriptorsFile = getStorageDescriptorsFile()
+        if (!descriptorsFile.exists()) {
+            return Collections.emptyList()
+        }
+
+        List<ProjectStorageDescriptor> result = new ArrayList<>()
+        GPathResult gPathResult = new XmlSlurper().parseText(FileUtils.read(descriptorsFile))
+        gPathResult.'storage'.each { result << new ProjectStorageDescriptor((String) it.@path, (String) it.@subDir) }
+
+        return result
+    }
+
+    void saveStorageDescriptors(List<ProjectStorageDescriptor> descriptors) {
+        // TODO: implement
+    }
+
+    File getOrCreateProjectStorageDir() {
+        String currentPath = ProjectHelper.currentProject.path
+
+        List<ProjectStorageDescriptor> descriptors = storageDescriptors
+
+        descriptors.each {
+            if (it.projectPath.equals(currentPath)) {
+                return getOrCreateProjectStorageDirBySubDir(it.storageSubDir)
+            }
+        }
+
+        ProjectStorageDescriptor projectStorageDescriptor = new ProjectStorageDescriptor(currentPath, StringUtils.generateId(8))
+        descriptors.add(0, projectStorageDescriptor)
+        saveStorageDescriptors(descriptors)
+
+        return getOrCreateProjectStorageDirBySubDir(projectStorageDescriptor.storageSubDir)
+    }
+
+    private File getOrCreateProjectStorageDirBySubDir(String subDirName) {
+        File dir = new File(getOrCreateColtDir(), "storage" + File.separator + subDirName)
+        if (!dir.exists()) {
+            dir.mkdirs()
+        }
+        return dir
+    }
+
+    public static void main(String[] args) {
+        new ProjectStorageManager().getStorageDescriptors().each { println(it.projectPath); println(it.storageSubDir) }
     }
 
 }
