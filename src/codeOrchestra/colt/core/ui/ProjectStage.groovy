c6180c7cc47c12e89fc3e3274042fa583bbf7c9b
Dima Kruk <dima.kruk@gmail.com>
2013-09-04 14:47:59 +0200

    fix layout

--- src/codeOrchestra/colt/core/ui/ProjectStage.groovy
+++ src/codeOrchestra/colt/core/ui/ProjectStage.groovy
@@ -1,15 +1,11 @@
 package codeOrchestra.colt.core.ui
 
-import codeOrchestra.colt.core.ColtProjectManager
-import codeOrchestra.colt.core.http.CodeOrchestraRPCHttpServer
-import codeOrchestra.colt.core.http.CodeOrchestraResourcesHttpServer
-import codeOrchestra.colt.core.loading.LiveCodingHandlerManager
 import codeOrchestra.colt.core.model.monitor.ChangingMonitor
 import codeOrchestra.colt.core.ui.dialog.ColtDialogs
-import codeOrchestra.lcs.license.ColtRunningKey
 import javafx.event.EventHandler
 import javafx.scene.Scene
 import javafx.scene.layout.Pane
+import javafx.scene.layout.StackPane
 import javafx.stage.Stage
 
 /**
@@ -20,7 +16,7 @@ class ProjectStage extends Stage {
     Pane root
 
     ProjectStage() {
-        root = new Pane()
+        root = new StackPane()
 
         setTitle("COLT — Code Orchestra Livecoding Tool (1.2)")
         scene = new Scene(root, 506, 820)
