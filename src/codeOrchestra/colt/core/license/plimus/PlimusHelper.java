f1c72d00f34922c319d24477286571b873268d5c
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-09-06 02:12:15 +0400

    [CJ-82]

--- src/codeOrchestra/colt/core/license/plimus/PlimusHelper.java
+++ src/codeOrchestra/colt/core/license/plimus/PlimusHelper.java
@@ -11,41 +11,44 @@ import java.io.IOException;
  */
 public final class PlimusHelper {
 
-  private static final int TIMEOUT = 1000;
+    private static final int SOCKET_READ_TIMEOUT = 1500;
+    private static final int CONNECTION_TIMEOUT = 2000;
 
-  private static final String VALIDATION_URL = "https://www.plimus.com/jsp/validateKey.jsp";  
-  private static final String PRODUCT_ID = "902584";
-  
+    private static final String VALIDATION_URL = "https://www.plimus.com/jsp/validateKey.jsp";
+    private static final String PRODUCT_ID = "902584";
+
 //  private static final String VALIDATION_URL = "https://sandbox.plimus.com/jsp/validateKey.jsp"; // sandbox
 //  private static final String PRODUCT_ID = "294006"; // sandbox
-  
-  private static HttpClient httpClient = new HttpClient();
 
-  private PlimusHelper() {
-  }
+    private static HttpClient httpClient = new HttpClient();
 
-  public static PlimusResponse registerKey(String key) throws IOException {
-    return executePlimusAction(key, PlimusValidationAction.REGISTER);
-  }
+    private PlimusHelper() {
+        httpClient.getHttpConnectionManager().getParams().setConnectionTimeout(CONNECTION_TIMEOUT);
+        httpClient.getHttpConnectionManager().getParams().setSoTimeout(SOCKET_READ_TIMEOUT);
+    }
 
-  public static PlimusResponse validateKey(String key) throws IOException {
-    return executePlimusAction(key, PlimusValidationAction.VALIDATE);
-  }
-  
-  private static PlimusResponse executePlimusAction(String key, PlimusValidationAction action) throws IOException {
-    GetMethod getMethod = new GetMethod(VALIDATION_URL);
+    public static PlimusResponse registerKey(String key) throws IOException {
+        return executePlimusAction(key, PlimusValidationAction.REGISTER);
+    }
 
-    getMethod.getParams().setParameter("http.socket.timeout", new Integer(TIMEOUT));
-    
-    getMethod.setQueryString(new NameValuePair[] {
-        new NameValuePair("action", action.name()),
-        new NameValuePair("productId", PRODUCT_ID),
-        new NameValuePair("key", key)
-    });
+    public static PlimusResponse validateKey(String key) throws IOException {
+        return executePlimusAction(key, PlimusValidationAction.VALIDATE);
+    }
 
-    httpClient.executeMethod(getMethod);
-    
-    return new PlimusResponse(getMethod.getResponseBodyAsString());
-  }
+    private static PlimusResponse executePlimusAction(String key, PlimusValidationAction action) throws IOException {
+        GetMethod getMethod = new GetMethod(VALIDATION_URL);
+
+        getMethod.getParams().setParameter("http.socket.timeout", Integer.valueOf(SOCKET_READ_TIMEOUT));
+
+        getMethod.setQueryString(new NameValuePair[]{
+                new NameValuePair("action", action.name()),
+                new NameValuePair("productId", PRODUCT_ID),
+                new NameValuePair("key", key)
+        });
+
+        httpClient.executeMethod(getMethod);
+
+        return new PlimusResponse(getMethod.getResponseBodyAsString());
+    }
 
 }
