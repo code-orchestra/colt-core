f1ef1294ada23d3bcc2bf47328e8ab91765701a2
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-09-09 10:43:13 +0200

    [CAS-453]

--- src/codeOrchestra/util/ApplicationUtil.groovy
+++ src/codeOrchestra/util/ApplicationUtil.groovy
@@ -4,11 +4,18 @@ import codeOrchestra.colt.core.ui.ColtApplication
 import javafx.application.Platform
 
 import java.awt.*
+import java.util.prefs.BackingStoreException
+import java.util.prefs.Preferences
 
 /**
  * @author Alexander Eliseyev
  */
 class ApplicationUtil {
+
+    private static final String LAST_START_REQUEST_KEY = "last_start_request"
+    private static final int REQUEST_TIMEOUT = 3000
+
+    private static Preferences preferences = Preferences.userNodeForPackage(ApplicationUtil.class)
 
     static void startAnotherColtInstance() throws IOException {
         if (SystemInfo.isMac) {
@@ -28,12 +35,25 @@ class ApplicationUtil {
             throw new IllegalStateException("Unsupported OS: " + System.getProperty("os.name"))
         }
 
-        ApplicationRestarter.start();
+        resetLastStartRequestDate()
+        ApplicationRestarter.start()
+    }
+
+    static boolean coltStartWasRecentlyRequested() {
+        long lastStartRequestTimestamp = preferences.getLong(LAST_START_REQUEST_KEY, 0)
+        return (System.currentTimeMillis() - lastStartRequestTimestamp) < REQUEST_TIMEOUT
+    }
+
+    private static void resetLastStartRequestDate() {
+        preferences.putLong(LAST_START_REQUEST_KEY, System.currentTimeMillis())
+        try {
+            preferences.sync()
+        } catch (BackingStoreException ignored) {
+        }
     }
 
     static void restartColt() throws IOException {
         startAnotherColtInstance()
-
         exitColt()
     }
 
