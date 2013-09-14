5456e9e7a2d2601addfca8b1f42ae587ba6e980c
Dima Kruk <dima.kruk@gmail.com>
2013-09-14 10:39:25 +0200

    [CAS-520] add method for convert

--- src/codeOrchestra/colt/core/ui/components/inputForms/base/ActionInputBase.groovy
+++ src/codeOrchestra/colt/core/ui/components/inputForms/base/ActionInputBase.groovy
@@ -1,13 +1,18 @@
 package codeOrchestra.colt.core.ui.components.inputForms.base
 
+import codeOrchestra.colt.core.model.Project
 import codeOrchestra.colt.core.ui.components.inputForms.markers.MAction
 import codeOrchestra.groovyfx.FXBindable
+import codeOrchestra.util.PathUtils
 import codeOrchestra.util.SystemInfo
+import javafx.beans.value.ChangeListener
+import javafx.beans.value.ObservableValue
 import javafx.event.ActionEvent
 import javafx.event.EventHandler
 import javafx.scene.control.Button
 import javafx.stage.DirectoryChooser
 import javafx.stage.FileChooser
+import javafx.util.StringConverter
 
 /**
  * @author Dima Kruk
@@ -34,18 +39,52 @@ abstract class ActionInputBase extends InputWithErrorBase implements MAction {
                     fileChooser.extensionFilters.addAll(extensionFilters)
                     File file = fileChooser.showOpenDialog(button.scene.window)
                     if (file) {
-                        textField.text = file.path
+                        text = file.path
                     }
                     break
                 case BrowseType.DIRECTORY:
                     DirectoryChooser directoryChooser = new DirectoryChooser()
                     File file = directoryChooser.showDialog(button.scene.window)
                     if (file) {
-                        textField.text = file.path
+                        text = file.path
                     }
                     break
             }
         } as EventHandler<ActionEvent>
+    }
+
+    void setShortPathForProject(Project project) {
+        textField.textProperty().unbindBidirectional(text())
+        text().addListener({ ObservableValue<? extends String> observableValue, String t, String t1 ->
+            println "t1 = $t1"
+        } as ChangeListener)
+        StringConverter converter = new StringConverter<String>() {
+            @Override
+            String toString(String t) {
+                String path = PathUtils.makeRelative(t, project)
+                if (path?.contains("\${project}")) {
+                    path = path.replace("\${project}/", "")
+                    println "path = $path"
+                }
+                return path
+            }
+
+            @Override
+            String fromString(String s) {
+                String result = s
+
+                File absolute = new File(PathUtils.makeAbsolute("\${project}/".concat(s)))
+                if (absolute.exists()) {
+                    result = absolute
+                }
+
+                return result
+            }
+        }
+        textField.textProperty().bindBidirectional(text(), converter)
+        textField.textProperty().addListener({ ObservableValue<? extends String> observableValue, String t, String newValue ->
+            textField.text = converter.toString(newValue)
+        } as ChangeListener)
     }
 
     void setButtonWidth(double value) {
