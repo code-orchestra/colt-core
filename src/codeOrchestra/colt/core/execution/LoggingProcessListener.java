af46b6f7a2b322d1a743e1ed2f7c03d72360d77e
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-09-17 16:47:16 +0200

    Logging improve

--- src/codeOrchestra/colt/core/execution/LoggingProcessListener.java
+++ src/codeOrchestra/colt/core/execution/LoggingProcessListener.java
@@ -1,32 +1,50 @@
 package codeOrchestra.colt.core.execution;
 
+import codeOrchestra.colt.core.logging.Level;
 import codeOrchestra.colt.core.logging.Logger;
 import codeOrchestra.util.process.ProcessAdapter;
 
+/**
+ * @author Alexander Eliseyev
+ */
 public class LoggingProcessListener extends ProcessAdapter {
 
-  private Logger logger;
-  
-  public LoggingProcessListener(String loggerName) {
-    logger = Logger.getLogger(loggerName);
-  }
-  
-  @Override
-  public void onTextAvailable(ProcessEvent event, String outputType) {                
-    String text = event.getText().trim();
-    
-    if ("finished.with.exit.code.text.message".equals(text)) {
-      logger.info("Finished with exit code " + event.getExitCode());
-      return;
+    private Logger logger;
+    private Level levelOverride;
+
+    public LoggingProcessListener(String loggerName, Level levelOverride) {
+        this.logger = Logger.getLogger(loggerName);
+        this.levelOverride = levelOverride;
     }
-    
-    if ("SYSTEM".equals(outputType)) {
-      logger.info(text);
-    } else if ("STDERR".equals(outputType)) {
-      logger.error(text);                  
-    } else {
-      logger.info(text);      
+
+    public LoggingProcessListener(String loggerName) {
+        this(loggerName, null);
     }
-  }
-  
+
+    @Override
+    public void onTextAvailable(ProcessEvent event, String outputType) {
+        String text = event.getText().trim();
+
+        if ("finished.with.exit.code.text.message".equals(text)) {
+            logger.live("Finished with exit code " + event.getExitCode());
+            return;
+        }
+
+        if (levelOverride != null) {
+            logger.log(text, levelOverride);
+        } else {
+            switch (outputType) {
+                case "SYSTEM":
+                    logger.info(text);
+                    break;
+                case "STDERR":
+                    logger.error(text);
+                    break;
+                default:
+                    logger.info(text);
+                    break;
+            }
+        }
+    }
+
 }
