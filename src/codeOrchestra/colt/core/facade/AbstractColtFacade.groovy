16138d3831573d0268c2409fd8f73f574b814994
Dima Kruk <dima.kruk@gmail.com>
2013-10-03 12:42:43 +0200

    [CJ-271] in progress

---
+++ src/codeOrchestra/colt/core/facade/AbstractColtFacade.groovy
@@ -0,0 +1,19 @@
+package codeOrchestra.colt.core.facade
+
+import codeOrchestra.colt.core.ui.ApplicationGUI
+
+/**
+ * @author Dima Kruk
+ */
+abstract class AbstractColtFacade implements ColtFacade {
+    protected ApplicationGUI applicationGUI
+
+    AbstractColtFacade(ApplicationGUI value) {
+        applicationGUI = value
+    }
+
+    @Override
+    void runSession() {
+        applicationGUI.runSession()
+    }
+}
