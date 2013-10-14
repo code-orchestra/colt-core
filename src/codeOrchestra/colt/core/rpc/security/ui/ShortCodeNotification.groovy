3be204dd5f1309d34c511ad10fa8c3d4146f3bc4
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-10-14 17:07:13 +0200

    FX threading issue fix

--- src/codeOrchestra/colt/core/rpc/security/ui/ShortCodeNotification.groovy
+++ src/codeOrchestra/colt/core/rpc/security/ui/ShortCodeNotification.groovy
@@ -2,6 +2,7 @@ package codeOrchestra.colt.core.rpc.security.ui
 
 import codeOrchestra.colt.core.rpc.security.ColtRemoteSecurityListener
 import codeOrchestra.colt.core.rpc.security.ColtRemoteSecurityManager
+import codeOrchestra.util.ThreadUtils
 import javafx.event.ActionEvent
 import javafx.geometry.Insets
 import javafx.scene.Node as FXNode
@@ -24,37 +25,41 @@ class ShortCodeNotification {
 
         ColtRemoteSecurityManager.instance.addListener(new ColtRemoteSecurityListener() {
             @Override
-            void onNewRequest(String requestor, String shortCode) {
-                TextFlow textFlow = new TextFlow()
-                textFlow.minWidth = Double.NEGATIVE_INFINITY
-                textFlow.padding = new Insets(7, 0, 0, 0)
-                textFlow.styleClass.add("notification-text")
-                textFlow.children.add(new Text("'" + requestor + "' has requested authorization to use COLT API.\nTo authorize, enter the following code into that tool: "))
-                Text shortCodeText = new Text(shortCode)
+            void onNewRequest(final String requestor, final String shortCode) {
+                ThreadUtils.executeInFXThread({
+                    TextFlow textFlow = new TextFlow()
+                    textFlow.minWidth = Double.NEGATIVE_INFINITY
+                    textFlow.padding = new Insets(7, 0, 0, 0)
+                    textFlow.styleClass.add("notification-text")
+                    textFlow.children.add(new Text("'" + requestor + "' has requested authorization to use COLT API.\nTo authorize, enter the following code into that tool: "))
+                    Text shortCodeText = new Text(shortCode)
 
-                shortCodeText.setFill(Color.BLUE)
-                textFlow.children.add(shortCodeText)
-                notificationPane.setGraphic(textFlow)
+                    shortCodeText.setFill(Color.BLUE)
+                    textFlow.children.add(shortCodeText)
+                    notificationPane.setGraphic(textFlow)
 
 
-                notificationPane.actions.clear()
-                AbstractAction action = new AbstractAction("Copy") {
-                    @Override
-                    void execute(ActionEvent actionEvent) {
-                        ClipboardContent clipboardContent = new ClipboardContent()
-                        clipboardContent.putString(shortCode)
-                        Clipboard.getSystemClipboard().setContent(clipboardContent)
+                    notificationPane.actions.clear()
+                    AbstractAction action = new AbstractAction("Copy") {
+                        @Override
+                        void execute(ActionEvent actionEvent) {
+                            ClipboardContent clipboardContent = new ClipboardContent()
+                            clipboardContent.putString(shortCode)
+                            Clipboard.getSystemClipboard().setContent(clipboardContent)
+                        }
                     }
-                }
-                notificationPane.actions.add(action)
-                notificationPane.show()
+                    notificationPane.actions.add(action)
+                    notificationPane.show()
+                } as Runnable)
             }
 
             @Override
             void onSuccessfulActivation(String shortCode) {
-                if (notificationPane.isShowing()) {
-                    notificationPane.hide()
-                }
+                ThreadUtils.executeInFXThread({
+                    if (notificationPane.isShowing()) {
+                        notificationPane.hide()
+                    }
+                } as Runnable)
             }
         })
 
