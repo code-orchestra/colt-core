8f328f8d0782d7e39da3ea2534bbd964704d8b9e
Dima Kruk <dima.kruk@codeorchestra.com>
2014-02-06 14:56:41 +0100

    [CJ-975] change compilation count and messages

--- src/codeOrchestra/colt/core/ui/dialog/SerialNumberDialog.groovy
+++ src/codeOrchestra/colt/core/ui/dialog/SerialNumberDialog.groovy
@@ -54,7 +54,7 @@ class SerialNumberDialog extends DialogWithImage {
         super.initHeader()
 
         message = "This is a demo version of COLT"
-        comment = "COLT is in Demo mode. AS compilations count is limited to 10, JS updates to 25"
+        comment = "COLT is in Demo mode. AS compilations count is limited to 25, JS updates to 25"
     }
 
     @Override
